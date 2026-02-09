5faf5953ae5ca895b4bedf08283b2d873734bfb5
Shreyansh443 <sanctuarycase22@gmail.com>
2026-02-09 14:15:20 +0530

    Add files via upload

---
+++ Day-3.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of elements in an array\n");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements inside the array\n");
+    for (int i = 0; i<n; i++){
+        scanf("%d", &arr[i]);
+
+    }
+    int k;
+    printf("Enter the number to be searched\n");
+    scanf("%d", &k);
+    int count=0;
+    for (int i = 0; i<n; i++){
+        if (arr[i]==k){
+            printf("Found at index %d\n", i);
+        }
+        count++;
+    }
+    printf("Comparisons = %d\n", count);
+    printf("%d, %d", arr[4], k);
+    return 0;
+}
