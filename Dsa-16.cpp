ed688d7dee30cd5cb41c5ef0b5e1762bc5f9b0e4
Shreyansh443 <sanctuarycase22@gmail.com>
2026-02-22 22:36:29 +0530

    Add files via upload

---
+++ Dsa-16.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int visited[n];   // to track counted elements
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        visited[i] = 0;   // initialize visited as 0
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(visited[i] == 1)
+            continue;
+
+        int count = 1;
+
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+                visited[j] = 1;  // mark as counted
+            }
+        }
+
+        printf("%d:%d\n", arr[i], count);
+    }
+
+    return 0;
+}
