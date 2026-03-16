55a139034857e5ceb7473bda588f9c9e097376a5
Shreyansh443 <sanctuarycase22@gmail.com>
2026-03-16 22:03:07 +0530

    Add files via upload

---
+++ Day-36.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node *front = NULL, *rear = NULL;
+
+// Enqueue
+void enqueue(int x) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+
+    if (front == NULL) {
+        front = rear = newNode;
+        newNode->next = newNode;  // circular
+    } else {
+        newNode->next = front;
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+// Dequeue
+void dequeue() {
+    if (front == NULL) return;
+
+    if (front == rear) {
+        free(front);
+        front = rear = NULL;
+    } else {
+        struct Node* temp = front;
+        front = front->next;
+        rear->next = front;
+        free(temp);
+    }
+}
+
+// Print queue
+void display() {
+    if (front == NULL) return;
+
+    struct Node* temp = front;
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != front);
+}
+
+int main() {
+    int n, m, x;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    scanf("%d", &m);
+
+    for (int i = 0; i < m; i++) {
+        dequeue();
+    }
+
+    display();
+
+    return 0;
+}
