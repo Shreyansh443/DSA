c683f289685d1ea55618326cae7dd661a987c6c3
Shreyansh443 <sanctuarycase22@gmail.com>
2026-04-25 21:51:14 +0530

    Add files via upload

---
+++ Day-74.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 1000
+#define LEN 50
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    char names[MAX][LEN];      // input names
+    char unique[MAX][LEN];     // unique candidates
+    int count[MAX] = {0};      // vote counts
+
+    // Read names
+    for (int i = 0; i < n; i++) {
+        scanf("%s", names[i]);
+    }
+
+    int uniqueCount = 0;
+
+    // Count votes
+    for (int i = 0; i < n; i++) {
+        int found = 0;
+
+        for (int j = 0; j < uniqueCount; j++) {
+            if (strcmp(names[i], unique[j]) == 0) {
+                count[j]++;
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found) {
+            strcpy(unique[uniqueCount], names[i]);
+            count[uniqueCount] = 1;
+            uniqueCount++;
+        }
+    }
+
+    // Find winner
+    int maxVotes = 0;
+    char winner[LEN] = "";
+
+    for (int i = 0; i < uniqueCount; i++) {
+        if (count[i] > maxVotes) {
+            maxVotes = count[i];
+            strcpy(winner, unique[i]);
+        }
+        else if (count[i] == maxVotes) {
+            if (strcmp(unique[i], winner) < 0) {
+                strcpy(winner, unique[i]);
+            }
+        }
+    }
+
+    printf("%s %d\n", winner, maxVotes);
+
+    return 0;
+}
